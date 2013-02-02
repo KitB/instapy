6c07e2efe165802a5bb2936938a633cf9312c33a
Kit Barnes <kit@ninjalith.com>
2013-02-02 17:27:12 +0000

    Make reloading somewhat possible
    Limits on reloading:
    * Can only reload modules, so "from ... import ..." will not work
    * Can't seem to reload modules imported in the function. Only global
      imports. Seems odd but will test further.
    * Much more I imagine

--- instapy.py
+++ instapy.py
@@ -1,17 +1,43 @@
 import imp
 import threading
+import sys
+import inspect
+import types
 
 
 class Reloader(threading.Thread):
     def __init__(self, main_function, a=None, kwa=None, *args, **kwargs):
+        super(Reloader, self).__init__(*args, **kwargs)
         self._n_reloads = 0
         self.args = a or tuple()
         self.kwargs = kwa or dict()
         self.main_function = main_function
-        super(Reloader, self).__init__(*args, **kwargs)
+        self.daemon = True
+        self.updated = False
+        self.running = False
+
+    def _loop(self):
+        while self.running:
+            if self.updated:
+                self._do_update()
+                self.updated = False
+            self.main_function(*self.args, **self.kwargs)
+        print "Thread exit"
 
     def run(self):
-        self.main_function(*self.args, **self.kwargs)
+        self.running = True
+        self._loop()
+
+    def _do_update(self):
+        print "Updating"
+        for k, v in self.main_function.func_globals.items():
+            if k != "__builtins__":
+                if isinstance(v, types.ModuleType):
+                    self.main_function.func_globals[k] = reload(v)
+                    print "%s, %s" % (k, isinstance(v, types.ModuleType))
+
+    def update(self):
+        self.updated = True
 
     def get_module(self, import_str):
         self._n_reloads += 1
