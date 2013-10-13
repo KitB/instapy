2819e01c156bba553391fd643c3236521ec49119
Kit Barnes <kit@ninjalith.com>
2013-10-13 13:48:13 +0100

    Make test.py take a class as an argument to run

--- test.py
+++ test.py
@@ -1,13 +1,24 @@
+import importlib
+import sys
+import time
+
 from instapy import reloader
 from instapy import watcher
 from watchdog import observers
-import time
 
-from examples.no_objects import interactive
+
+def get_looper(looper_string):
+    (module_name, dot, cls_name) = looper_string.rpartition('.')
+    module = importlib.import_module(module_name)
+    cls = module.__dict__[cls_name]
+    return cls()
 
 
 if __name__ == "__main__":
-    r = reloader.LooperReloader(interactive.Game())
+    # We give a class as 'module.submodule.ClassName' and it'll run it. How
+    # fancy!
+    looper = get_looper(sys.argv[1])
+    r = reloader.LooperReloader(looper)
     handler = watcher.Notifier(r)
     o = observers.Observer()
     o.schedule(handler, path='.', recursive=True)
