76697daff548a7cb970de94d08c38cb34c897ea7
Kit Barnes <kit@ninjalith.com>
2013-12-05 21:26:38 +0000

    Remove object tracking behaviour, replace with argument tracking
    The injector now causes objects to remember the arguments they were initialised
    with. It no longer does any tracking of objects overall; though it *may* be
    readded in future.

--- instapy/inject.py
+++ instapy/inject.py
@@ -1,35 +1,29 @@
-""" Replacing the builtin object class with a class that will track all objects
-    created.
+""" A mechanism for causing objects to track their initialisation arguments.
 
-    The important advantage of this over using gc.get_objects() is that we can
-    determine a portion of code in which to modify classes such that they are
-    tracked. For instapy this means we won't be influencing builtins or portions
-    of instapy.
-
-    On the other hand such behaviour could be achieved by other means. Possibly
-    using decorators allowing the programmer to tag things as updatable. Such a
-    mechanism would avoid the fairly significant drawbacks of using weakrefs to
-    avoid a memory leak here."""
+    This is performed by adding a metaclass to the builtin object class."""
 import __builtin__
-import weakref
 
-# NOTE: There are a whole bunch of things that cannot be stored in this.
-tracked_objects = weakref.WeakSet()
 
 original_object = object
 
 
-class TrackingObject(object):
-    def __new__(cls, *args, **kwargs):
-        i = super(TrackingObject, cls).__new__(cls, *args, **kwargs)
-        tracked_objects.add(i)
+class TrackingMeta(type):
+    def __call__(cls, *args, **kwargs):
+        i = type.__call__(cls, *args, **kwargs)
         return i
 
 
+class TrackingObject(object):
+    __metaclass__ = TrackingMeta
+
+
 class ReplacedContext(object):
+    def __init__(self, new_object=TrackingObject):
+        self.new_object = new_object
+
     def __enter__(self):
         self.old_object = object
-        __builtin__.object = TrackingObject
+        __builtin__.object = self.new_object
 
     def __exit__(self, exc_type, exc_value, traceback):
         __builtin__.object = self.old_object
